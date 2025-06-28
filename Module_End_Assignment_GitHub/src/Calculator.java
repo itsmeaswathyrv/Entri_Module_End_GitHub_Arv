de702e99027ba6ebf80883c643e0612cf623b49f
Aswathy <itsmeaswathyrv@gmail.com>
2025-06-28 18:12:51 +0530

    Initial commit of project - Module_End_Assignment_GitHub

---
+++ Module_End_Assignment_GitHub/src/Calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter 2 numbers: ");
+		Scanner in = new Scanner(System.in);
+		int num1 = in.nextInt();
+		int num2 = in.nextInt();
+		
+		System.out.println("Enter the desired mathematical operator (+, -, *, /, %): ");
+		char op = in.next().charAt(0);
+		
+		switch (op) {
+		case '+':
+			System.out.println("Sum of the given numbers is "+(num1+num2));
+			break;
+			
+		case '-':
+			System.out.println("Sum of the given numbers is "+(num1-num2));
+			break;
+			
+		case '*':
+			System.out.println("Sum of the given numbers is "+(num1*num2));
+			break;
+			
+		case '/':
+			System.out.println("Sum of the given numbers is "+(num1/num2));
+			break;
+		
+		case '%':
+			System.out.println("Sum of the given numbers is "+(num1%num2));
+			break;
+			
+		default:
+			System.out.println("Please enter a valid operator!");
+
+		}
+	}
+
+}
